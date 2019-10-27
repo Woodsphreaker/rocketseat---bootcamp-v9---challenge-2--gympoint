f4df4b77688208357d20a0ae4f7071a95e73df98
Carlo Enrico <carlo.enrico@uol.com.br>
2019-10-27 00:49:35 -0300

    Update: Add Yup

--- src/app/controllers/sessionsController.js
+++ src/app/controllers/sessionsController.js
@@ -1,9 +1,18 @@
 import jwt from 'jsonwebtoken'
 import auth from '../../config/auth'
 import bcrypt from 'bcrypt'
+import * as Yup from 'yup'
 import Users from '../models/Users'
 
 const store = async (req, res, next) => {
+  const schema = Yup.object().shape({
+    name: Yup.string().required(),
+    password: Yup.string().required(),
+  })
+
+  if (!(await schema.isValid(req.body))) {
+    return res.status(401).json({ error: 'Required fields are missing' })
+  }
   const { email, password } = req.body
 
   const user = await Users.findOne({ where: { email } })
